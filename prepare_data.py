65dce1e41525ff0de7da2759b9b276682b0af5ef
Charles-Emmanuel Dias <Charles-Emmanuel.Dias@etu.upmc.fr>
2017-12-12 13:14:54 +0100

    prepare_data changed

--- prepare_data.py
+++ prepare_data.py
@@ -1,21 +1,17 @@
-import spacy
+#import spacy
 import gzip
 import argparse
-import gensim
 import logging
 import json
 import pickle as pkl
+import spacy
+import itertools
 
 from tqdm import tqdm
 from random import randint,shuffle
 from collections import Counter, Iterator
 
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO) #gensim logging
-
-#Global dicts for users
-USERS = {}
-ITEMS = {}
 
 def count_lines(file):
     count = 0
@@ -27,77 +23,29 @@ def count_lines(file):
 
 def build_dataset(args):
 
-    def preprocess(datas):
-        for data in datas:
-
-            uk = USERS.setdefault(data["reviewerID"], len(USERS))
-            ik = ITEMS.setdefault(data["asin"], len(ITEMS))
-
-            yield (uk,ik,data['reviewText'],max(1,int(round(float(data["overall"]))))-1) #zero is useless, classes between 0-4 for 1-5 reviews
-
-    def preprocess_rescale(datas):
-        for data in datas:
-
-            uk = USERS.setdefault(data["reviewerID"], len(USERS))
-            ik = ITEMS.setdefault(data["asin"], len(ITEMS))
-
-            rating = max(1,int(round(float(data["overall"]))))-1
-
-            if rating > 3:
-                rating = 1
-            elif rating == 3:
-                yield None
-                continue
-            else:
-                rating = 0
-            yield (uk, ik, data['reviewText'],rating) #zero is useless
+    def to_array_comp(doc):
+        return [[w.orth_ for w in s] for s in doc.sents]
 
     def data_generator(data):
         with gzip.open(args.input,"r") as f:
             for x in tqdm(f,desc="Reviews",total=count_lines(f)):
                 yield json.loads(x)
 
-    class TokIt(Iterator):
-        def __init__(self, tokenized):
-            self.tok = tokenized
-            self.x = 0
-            self.stop = len(tokenized)
-
-        def __iter__(self):
-            return self
-
-        def next(self):
-            if self.x < self.stop:
-                self.x += 1
-                return list(w.orth_ for w in self.tok[self.x-1] if len(w.orth_.strip()) >= 1 ) #whitespace shouldn't be a word.
-            else:
-                self.x = 0
-                raise StopIteration 
-        __next__ = next
-
-
-
+    def extract_w(toked):
+        return [[w.orth_ for w in s] for s in toked.sents]
 
     print("Building dataset from : {}".format(args.input))
     print("-> Building {} random splits".format(args.nb_splits))
 
-    nlp = spacy.load('en')
+    def custom_pipeline(nlp):
+        return (nlp.tagger, nlp.parser,to_array_comp)
 
-    tokenized = [tok for tok in tqdm(nlp.tokenizer.pipe((x["reviewText"] for x in data_generator(args.input)), batch_size=10000, n_threads=8),desc="Tokenizing")]
+    nlp = spacy.load('en', create_pipeline=custom_pipeline)
+    gen_a,gen_b = itertools.tee(data_generator(args.input),2)
 
-    
+    data = [(z["reviewerID"],z["asin"],tok,z["overall"]) for z,tok in zip(tqdm((z for z in gen_a),desc="reading file"),nlp.pipe((x["reviewText"] for x in gen_b), batch_size=1000000, n_threads=8))]
 
-    if args.create_emb:
-        w2vmodel = gensim.models.Word2Vec(TokIt(tokenized), size=args.emb_size, window=5, min_count=5, iter=args.epochs, max_vocab_size=args.dic_size, workers=4)
-        print(len(w2vmodel.wv.vocab))
-        w2vmodel.wv.save_word2vec_format(args.emb_file,total_vec=len(w2vmodel.wv.vocab))        
-
-    if args.rescale:
-        print("-> Rescaling data to 0-1 (3's are discarded)")
-        data = [dt for dt in tqdm(preprocess_rescale(data_generator(args.input)),desc="Processing") if dt is not None]
-    else:
-        data = [dt for dt in tqdm(preprocess(data_generator(args.input)),desc="Processing")]
-
+    print(data[0])
     shuffle(data)
 
     splits = [randint(0,args.nb_splits-1) for _ in range(0,len(data))]
@@ -118,7 +66,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=str)
     parser.add_argument("output", type=str, default="sentences.pkl")
-    parser.add_argument("--rescale",action="store_true")
     parser.add_argument("--nb_splits",type=int, default=5)
 
     parser.add_argument("--create-emb",action="store_true")
@@ -128,7 +75,5 @@ if __name__ == '__main__':
     parser.add_argument("--epochs", type=int,default=1)
     args = parser.parse_args()
 
-    if args.emb_file is None:
-        args.emb_file = args.output + "_emb.txt"
 
     main(args)
